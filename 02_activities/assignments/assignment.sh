f828fe6d580138effb591a713b9b5b47f262c706
NamreenSyed <namreen.syed@utoronto.ca>
2024-08-31 23:42:14 -0400

    Assignment-1

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -23,13 +23,12 @@ touch dir2/file3.txt
 touch dir2/file4.txt
 touch dir2/file5.txt
 # 4. Append the words "Hello world" to dir2/file3
-echo "Hello world" >> file3.txt
+echo "Hello world" >> dir2/file3.txt
 # 5. Verify that file3 contains the words "hello world" by printing the contents of the file in the terminal
 cat dir2/file3.txt
 # 6. Delete file4
 rm dir2/file4.txt
 # 7. Delete directories dir4 and dir5 including all their contents (if any)
-cd ..
 rm -dr dir4 dir5
 # 8. List the contents of the parent directory to verify the deletion of dir4 and dir5
-ls
+ls my_directory
